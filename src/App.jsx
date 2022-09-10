f684a0b07e64b77b0d8faad160547674f6689e46
Samarth Singh <samarthsingh6358@gmail.com>
2022-09-10 21:06:23 +0530

    board and square components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
+import Board from './components/Board';
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to React Vite Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
-    </>
+    <div>
+      <h1> Tic Tac Toe </h1>
+      <Board />
+    </div>
   );
 };
 
