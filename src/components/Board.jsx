ca676bf9530cb7022d2e74d920371531a39ed580
Samarth Singh <samarthsingh6358@gmail.com>
2022-09-10 23:43:46 +0530

    added board state and onClick event

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 const Board = () => {
+  const [board, update_state] = useState(Array(9).fill(null));
+  const [isXnext, setXnext] = useState(false);
+
+  const handlesquareclick = position => {
+    if (board[position]) return;
+    update_state(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXnext ? 'O' : 'X';
+        }
+        return square;
+      });
+    });
+    setXnext(prev => !prev);
+  };
+  const renderSquare = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => handlesquareclick(position)}
+      />
+    );
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
